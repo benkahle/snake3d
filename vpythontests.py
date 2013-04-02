f00bdc610e780d4e185d5546ffe89d76c0b0cd2f
benkahle <ben@kahle.org>
2013-04-01 21:19:42 -0700

    A few additions in the 3rd dimension.

--- vpythontests.py
+++ vpythontests.py
@@ -2,7 +2,8 @@ from visual import *
 
 dt = 0.001
 
-border = curve(pos=[(-100,-100),(100,-100),(100,100),(-100,100),(-100,-100)])
+border = curve(pos=[(-100,-100,100),(100,-100,100),(100,-100,-100),(100,100,-100),(-100,100,-100),(-100,100,100),(-100,-100,100),(-100,-100,-100),(-100,100,-100),(-100,-100,-100),(100,-100,-100),(100,-100,100),(100,100,100),(100,100,-100),(100,100,100),(-100,100,100)])
+#grid1 = curve(pos=[(-100,-100,z)])
 snake = box(pos=(6,0,0), length=4, width=4, height=4, color=color.red)
 snake.v = vector(0,0,0)
 snake.origin = (0,0,0)
@@ -14,29 +15,41 @@ def check_dir(snake):
         if key == 'left':
             snake.v[0]=-1
             snake.v[1]=0
+            snake.v[2]=0
         if key == 'right':
             snake.v[0]=1
             snake.v[1]=0
+            snake.v[2]=0
         if key == 'up':
             snake.v[1]=1
             snake.v[0]=0
+            snake.v[2]=0
         if key == 'down':
             snake.v[1]=-1
             snake.v[0]=0
-def bounce(snake):
-    print('bounce check:')
+            snake.v[2]=0
+        if key == 'w':
+            snake.v[2]=1
+            snake.v[0]=0
+            snake.v[1]=0
+        if key == 's':
+            snake.v[2]=-1
+            snake.v[0]=0
+            snake.v[1]=0
+def check_wall(snake):
     if snake.pos[0]<= -100 or snake.pos[0]>= 100:
-        print('x-error')
-        snake.v[0] == -snake.v[0]
-    if snake.pos[1]<= -100 or snake.pos[1]>= 100:
-        print('y-error')
-        snake.v[1] == -snake.v[1]
+        return False
+    elif snake.pos[1]<= -100 or snake.pos[1]>= 100:
+        return False
+    else: return True
 def one_tick(snake):
-    check_dir(snake)
-    bounce(snake)
-    snake.pos += snake.v*dt
-
-        
+    if check_wall(snake):
+        check_dir(snake)
+        snake.pos += snake.v*dt
+    else:
+        pass
+        #endtext = text(text='Game Over!', align='center',pos=[0,0,0], color=color.red)
+        #This should work, but isn't for some reason
 
 while 1:
     one_tick(snake)
