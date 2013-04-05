01211ff8fc163676752dfe4d076333be44870c39
cullenross <cullen.ross@students.olin.edu>
2013-04-04 18:00:20 -0700

    Added speed increase after every food eaten

--- vpythontests.py
+++ vpythontests.py
@@ -4,8 +4,12 @@ import random
 class Foodbox(object):
     def __init__(self):
         self.food=[]
+class Timestep(object):
+    def __init__(self):
+        self.timestep=[]
 
-dt = 0.01
+dt = Timestep()
+dt.timestep=0.01
 
 #scene = display(title='Super-Mega Snake Game')
 scene.exit=1
@@ -16,13 +20,13 @@ snake.v = vector(0,0,0)
 zbox = curve(pos = [(-100,-100,snake.pos[2]),(100,-100,snake.pos[2]),(100,100,snake.pos[2]),(-100,100,snake.pos[2]),(-100,-100,snake.pos[2])], color = color.yellow)
 scene.autoscale = False
 foodbox=Foodbox()
-food = box(pos=(50,0,0), length=2, width=2, height=2, color=color.cyan)
+food = box(pos=(random.randint(-96,96),random.randint(-96,96),random.randint(-96,96)), length=2, width=2, height=2, color=color.cyan)
 foodbox.food.append(food)
 foodsquare = curve(pos = [(-100,-100,food.pos[2]),(100,-100,food.pos[2]),(100,100,food.pos[2]),(-100,100,food.pos[2]),(-100,-100,food.pos[2])], color = color.green)
 
 #counter = 0
 def randpos(thingy):
-    thingy.pos=(random.randint(-100,100),random.randint(-100,100),random.randint(-100,100))
+    thingy.pos=(random.randint(-98,98),random.randint(-98,98),random.randint(-98,98))
 def check_dir(snake):
     if scene.kb.keys: # is there an evcd UnicodeDecodeError()ent waiting to be processed?
         key = scene.kb.getkey() # obtain keyboard information
@@ -49,17 +53,18 @@ def check_wall(snake):
 def zboxmove(snake):
     zbox.pos = [(-100,-100,snake.pos[2]),(100,-100,snake.pos[2]),(100,100,snake.pos[2]),(-100,100,snake.pos[2]),(-100,-100,snake.pos[2])]
 def checkfood():
-    n=8
+    n=3
     for food in foodbox.food:
         if abs(snake.pos[0]-food.pos[0])<=n and abs(snake.pos[1]-food.pos[1])<=n and abs(snake.pos[2]-food.pos[2])<=n:
             randpos(food)
             foodsquare.pos = [(-100,-100,food.pos[2]),(100,-100,food.pos[2]),(100,100,food.pos[2]),(-100,100,food.pos[2]),(-100,-100,food.pos[2])]
+            dt.timestep+=.003
 def one_tick(snake):
 #    if check_wall(snake):
     checkfood()
     check_dir(snake)
     zboxmove(snake)
-    snake.pos += snake.v*dt
+    snake.pos += snake.v*dt.timestep
 def end(): 
     endtext = label(text='Game Over!', align='center',pos=[0,0,0], height = 30, color=color.red)
     options = label(text = 'Press P to play again', align = 'center', pos = [0,-50], color=color.yellow, height=10)
