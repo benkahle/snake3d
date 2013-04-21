b57d0f7cfe142f246c8f8130a501ea96b1980cd9
benkahle <ben@kahle.org>
2013-04-21 10:53:38 -0700

    Made some progress on networked implementation of snake.

--- pyrotests/gametests/snakeclient.py
+++ pyrotests/gametests/snakeclient.py
@@ -1,14 +1,15 @@
 import socket
-import select
+import select as sel
 import random
 import time
+from visual import *
 
 class SnakeClient(object):
-  def __init__(self, addr="192.168.134.149", serverport=9009):
-    self.clientport = random.randrange(8000, 8999)
+  def __init__(self, addr="192.168.134.151", serverport=9008):
+    self.clientport = random.randint(8000, 8999)
     self.conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # Bind to localhost - set to external ip to connect from other computers
-    self.conn.bind(("192.168.134.149", self.clientport))
+    self.conn.bind(("192.168.134.151", self.clientport))
     self.addr = addr
     self.serverport = serverport
     self.read_list = [self.conn]
@@ -16,13 +17,18 @@ class SnakeClient(object):
     
     self.setup_game()
   
-  def setup_pygame(self):
+  def setup_game(self):
     #Setup vpython game world here
+    scene = display(title='Super-Mega Snake Game', width=250, height=250)
+    border = curve(pos=[(-100,-100),(100,-100),(100,100),(-100,100)])
+    scene.autoscale = False
+    snake = box(pos=(0,0), length=4, width=4, height=4, color=color.red)
 
   def check_keyinput(self):
+    cmd = ' '
     if scene.kb.keys: # is there an evcd UnicodeDecodeError()ent waiting to be processed?
-      welcome.visible=0
-      welcomebox.visible=0
+      # welcome.visible=0
+      # welcomebox.visible=0
       key = scene.kb.getkey() # obtain keyboard information
       if key == 'left':
         cmd = 'a'
@@ -32,33 +38,36 @@ class SnakeClient(object):
         cmd = 'c'
       if key == 'down':
         cmd = 'd'
-      if key == 's':
-        cmd = 'e'
-      if key == 'w': 
-        cmd = 'f'
+      # if key == 's':
+      #   cmd = 'e'
+      # if key == 'w': 
+      #   cmd = 'f'
     return cmd
   
   def run(self):
     running = True
 
     try:
+      time.sleep(1)
       # Initialize connection to server
       self.conn.sendto("c", (self.addr, self.serverport))
       while running:
-        #'Wait' function for discrete time
+        #'Wait' function for discrete time?
         
         # select on specified file descriptors
-        readable, writable, exceptional=(select.select(self.read_list, self.write_list, [], 0))
+        readable, writable, exceptional=(sel.select(self.read_list, self.write_list, []))
         for f in readable:
           if f is self.conn: #if a packet is received
             msg, addr = f.recvfrom(32)
+            print(msg,addr)
             for position in msg[:2].split('|'):
-              x, sep, y, sep, z = position.partition(',')
+              x, sep, y = position.partition(',')
               try:
-                #Update position of each snake
+                snake.pos =(x,y)
               except:
                 pass  # If something goes wrong, don't draw anything.
-        self.conn.sendto("u"+self.check_keyinput, (self.addr, self.serverport))
+        print('u'+str(self.check_keyinput()))
+        self.conn.sendto("u"+str(self.check_keyinput()), (self.addr, self.serverport))
     finally:
       self.conn.sendto("d", (self.addr, self.serverport))
 
--- pyrotests/gametests/snakeserver.py
+++ pyrotests/gametests/snakeserver.py
@@ -1,75 +1,82 @@
 import socket
-import select
+import select as sel
 import sys
 
 class SnakeServer(object):
-    def __init__(self,port = 9009):
-        self.listener = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-        self.listener.bind(('192.168.134.149',9009))
-        self.read_list = [self.listener] #receiving client packets here
-        self.write_list = [] #List to send to clients
-        self.players = {} #stored position and velocity by address
+	def __init__(self,port = 9008):
+		self.listener = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+		self.listener.bind(('192.168.134.151',9008))
+		self.read_list = [self.listener] #receiving client packets here
+		self.write_list = [] #List to send to clients
+		self.players = {} #stored position and velocity by address
 
-    def do_movement(self,mv,player):
-        vel=self.players[player][1]
-        if mv = 'a':
-            #set V(x+)
-        if mv = 'b':
-            #set V(x-)
-        if mv = 'c':
-            #set V(y+)
-        if mv = 'd':
-            #set V(y-)
-        if mv = 'e':
-            #set V(z+)
-        if mv = 'f':
-            #set V(z-)
-        self.players[player][1]=vel
-        self.change_pos(player)
+	def do_movement(self,mv,player):
+		vel=self.players[player][1]
+		if mv == ' ':
+			vel == vel
+		if mv == 'a':
+			vel = (3,0)
+		if mv == 'b':
+			vel = (-3,0)
+		if mv == 'c':
+			vel = (0,3)
+		if mv == 'd':
+			vel = (0,-3)
+		if mv == 'e':
+			#set V(z+)
+			pass
+		if mv == 'f':
+			pass
+			#set V(z-)
+		self.players[player][1]=vel
+		self.change_pos(player)
 
-    def change_pos(self,player):
-        pos=self.players[player][0]
-        #update player position with current velocity
-        self.players[player][0]=pos
+	def change_pos(self,player):
+		pos=self.players[player][0]
+		vel = self.players[player][1]
+		pos = pos + vel
+		self.players[player][0]=pos
 
-    def check_death(self,played):
-        pos = self.players[player][0]
-        #Check for collisions
-        #if collision:
-        #    return True    
-        #else:
-        #    return False
-    
-    def run(self):
-        print('waiting...')
-        try:
-            while True:
-                readable,writable,exceptional = (select.select(self.read_list,self.write_list,[]))
-                for f in readable:
-                    if f is self.listener:
-                        msg,addr = f.recvfrom(32)
-                        if len(msg) >= 1:
-                            cmd = msg[0]
-                            if cmd == 'c': #New connection
-                                self.players[addr] = ((0,0,0),(0,0,0)) #((pos),(vel))
-                            elif cmd == 'u': #Movement update
-                                if len(msg) >= 2 and addr in self.players:
-                                    self.do_movement(msg[1],addr)
-                            elif cmd == 'd':
-                                if addr in self.players:
-                                    del self.players[addr]
-                            else:
-                                print("unexpected: {0}".format(msg))
-                for player in self.players:
-                    running_state = self.check_death(player)
-                    send = []
-                    for pos in self.players:
-                        send.append("{0}{1}{2}".format(*self.players[pos][0]))
-                    self.append(runningstate)
-                    self.listener.sendto('|'.join(send),player)
-        except KeyboardInterrupt as e:
-            pass
-            
+	def check_death(self,player):
+		pos = self.players[player][0]
+		#Check for collisions
+		#if collision:
+		#    return True    
+		#else:
+		#    return False
+		return True #Temp
+	
+	def run(self):
+		print('waiting...')
+		try:
+			while True:
+				readable,writable,exceptional = (sel.select(self.read_list,self.write_list,[]))
+				for f in readable:
+					if f is self.listener:
+						msg,addr = f.recvfrom(32)
+						if len(msg) >= 1:
+							cmd = msg[0]
+							if cmd == 'c': #New connection
+								self.players[addr] = [(0,0),(0,0)] #((pos),(vel))
+							elif cmd == 'u': #Movement update
+								if len(msg) >= 2 and addr in self.players:
+									self.do_movement(msg[1],addr)
+							elif cmd == 'd':
+								if addr in self.players:
+									del self.players[addr]
+							else:
+								print("unexpected: {0}".format(msg))
+				for player in self.players:
+					running_state = self.check_death(player)
+					send = []
+					for pos in self.players:
+						send.append("{0}{1}".format(*self.players[pos][0]))
+					send.append(str(running_state))
+					self.listener.sendto('|'.join(send),player)
+					print(send)
+		except KeyboardInterrupt as e:
+			pass
+
 if __name__ == '__main__':
-    g = SnakeServer()
-    g.run()
+	g = SnakeServer()
+	g.run()
