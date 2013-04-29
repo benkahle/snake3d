01c420c785cbb1dab2c341737a5385d57fbf559e
benkahle <ben@kahle.org>
2013-04-29 14:37:03 -0400

    Test files for networking.

--- pyrotests/gametests/snakeserver.py
+++ pyrotests/gametests/snakeserver.py
@@ -6,7 +6,7 @@ from visual import *
 class SnakeServer(object):
 	def __init__(self,port = 9007):
 		self.listener = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-		self.listener.bind(('192.168.134.152',9007))
+		self.listener.bind(('127.0.0.1',9007))
 		self.read_list = [self.listener] #receiving client packets here
 		self.write_list = [] #List to send to clients
 		self.players = {} #stored position and velocity by address
