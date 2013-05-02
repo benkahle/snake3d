0020a57ed44064bbe89b7dcdd0e669560321cd10
CameronAnderson <cameron.anderson@students.olin.edu>
2013-05-02 14:48:39 -0400

    completed picking between modes

---
+++ pyrotests/gametests/initiateplay.py
@@ -0,0 +1,11 @@
+import os
+
+
+x = input('Welcome to Super Mega Snake. Please press 1 for single player, 2 for double player, and 3 for networked play.')
+
+if x == 1:
+	os.system('python vpythontests.py')
+elif x == 2:
+	os.system('python battlesnake.py')
+elif x == 3:
+	os.system('python snakeclient.py')
