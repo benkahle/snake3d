411d7ec49eb13402e95d09a989e27801e06f3665
benkahle <ben@kahle.org>
2013-05-08 12:09:54 -0400

    Ip settings for final demo.

--- pyrotests/gametests/base_snakeserver.py
+++ pyrotests/gametests/base_snakeserver.py
@@ -7,7 +7,7 @@ from visual import *
 class SnakeServer(object):
 	def __init__(self,port = 55555):
 		self.listener = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-		self.listener.bind(('10.41.64.143',55555))
+		self.listener.bind(('10.41.24.109',55555))
 		self.read_list = [self.listener] #receiving client packets here
 		self.write_list = [] #List to send to clients
 		self.players = {} #stored position and velocity by address
