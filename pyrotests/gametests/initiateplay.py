4711f55f66b281cf131809fb9fcac856d80d8653
cullenross <cullen.ross@students.olin.edu>
2013-05-12 21:02:14 -0700

    added hosting options

--- pyrotests/gametests/initiateplay.py
+++ pyrotests/gametests/initiateplay.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from visual import *
 from visual.controls import *
 
@@ -6,23 +7,39 @@ from visual.controls import *
 scene = display(width=750, height=750)
 welcome = label(text='Welcome to Super-Mega Snake Game!',align='center',pos=(0,0,0), yoffset= 100, height = 25, color = color.magenta)
 instructions = label(text='Please click one of the options below.',align='center',pos=(0,0,0),yoffset= 73, height = 10)
-
-#blarg = curve(pos = [(-35,-63,0),(35,-34,0),(34,-44,0),(22,-15,0),(-100,-100,0)])
+scene.autoscale = 0
 
 #DON'T CHANGE ANY OF THESE POSITIONS. Or any of the text. For real.
 one = label(text = 'Single Player Mode', align='center',pos=(0,0,0), yoffset= 0,color = color.yellow)
 two = label(text = 'Two Player Mode', align='center',pos=(0,0,0), yoffset= -55,color = color.yellow)
-twodnet = label(text = 'Join a 2D Network Game', align='center',pos=(0,0,0), yoffset= -125, color = color.yellow)
-threednet = label(text = 'Join a 3D Network Game', align='center',pos=(0,0,0), yoffset= -195, color = color.yellow)
+twodnet = label(text = 'Join a 2D Network Game', align='center', xoffset=-135, pos=(-.5,0,0),yoffset= -135, color = color.yellow)
+threednet = label(text = 'Join a 3D Network Game', align='center',xoffset=-150,pos=(0,0,0), yoffset= -205, color = color.yellow)
+twohost = label(text = 'Host a 2D Network Game', align='center', xoffset=135, pos=(.5,0,0),yoffset= -135, color = color.yellow)
+threehost = label(text = 'Host a 3D Network Game', align='center', xoffset=150, pos=(0,0,0),  yoffset= -205, color = color.yellow)
+
+
+
 
 while 1:
 	if scene.mouse.clicked:
 		m = scene.mouse.getclick()
+
+		#locals
 		if m.pos[0]>=-2.85 and m.pos[0]<=2.85 and m.pos[1]>=-.51 and m.pos[1]<=.51:
 			os.system('python vpythontests.py')
 		elif m.pos[0]>=-2.5 and m.pos[0]<=2.5 and m.pos[1]>=-2.8 and m.pos[1]<=-1.75:
 			os.system('python battlesnake.py')
-		elif m.pos[0]>=-3.94 and m.pos[0]<=3.94 and m.pos[1]>=-5.04 and m.pos[1]<=-4.02:
+		
+		#joins
+		elif m.pos[0]>=-8.43 and m.pos[0]<=-1.227 and m.pos[1]>=-5.355 and m.pos[1]<=-4.3:
 			os.system('python base_snakeclient.py')
-		elif m.pos[0]>=-3.94 and m.pos[0]<=3.94 and m.pos[1]>=-7.24 and m.pos[1]<=-6.18:		
+		elif m.pos[0]>=-8.43 and m.pos[0]<=-1.227 and m.pos[1]>=-7.62 and m.pos[1]<=-6.567:		
+			os.system('python 3d_snakeclient.py')
+		
+		#hosts
+		elif m.pos[0]>=1.1 and m.pos[0]<=8.53 and m.pos[1]>=-5.355 and m.pos[1]<=-4.3:
+			os.system('python base_snakeserver.py')
+			os.system('python base_snakeclient.py')
+		elif m.pos[0]>=1.1 and m.pos[0]<=8.53 and m.pos[1]>=-7.62 and m.pos[1]<=-6.567:		
+			os.system('python 3d_snakeserver.py')
 			os.system('python 3d_snakeclient.py')
